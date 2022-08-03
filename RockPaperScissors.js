3c3d047bd32cac7a0a9b14c7074fe959d77bca3e
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2022-08-03 13:19:34 +0100

    Update RockPaperScissors.js

---
+++ RockPaperScissors.js
@@ -0,0 +1,12 @@
+let options = ["rock","paper","scissors"];
+let computerSelection = options[Math.floor(Math.random() * options.length)];
+let playerSelection = "rock";
+
+
+function Playround(playerSelection)
+{
+console.log('It is a draw')
+}
+
+
+Playround(playerSelection);
