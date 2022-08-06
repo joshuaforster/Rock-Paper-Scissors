9f16470a23351d296b683323178a53f3d2727ee3
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2022-08-06 10:52:17 +0100

    Update RockPaperScissors.js
    Completed js only console game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,12 +1,32 @@
-let options = ["rock","paper","scissors"];
-let computerSelection = options[Math.floor(Math.random() * options.length)];
-let playerSelection = "rock";
+let num1 = (Math.random());
+let playerSelection = 'rock'
+let computerChoice = computerSelection();
 
+gameplay()
+function computerSelection() {
+if (num1 <= 0.333) {return 'rock'}
+else if (num1 > 0.333 && num1 <= 0.666) {return 'paper'}
+else {return 'scissors'}
 
-function Playround(playerSelection)
-{
-console.log('It is a draw')
 }
 
-
-Playround(playerSelection);
+function gameplay(){
+  if(computerChoice ==='rock' && playerSelection == 'rock')
+  {console.log('You both chose rock, it is a draw')}
+  else if (computerChoice ==='paper' && playerSelection =='rock')
+  {console.log('You lose, paper wraps rock')}
+  else if (computerChoice ==='scissors' && playerSelection =='rock')
+  {console.log('You win, rock breaks scissors')}
+  else if (computerChoice ==='rock' && playerSelection =='paper')
+  {console.log('You win, paper wraps rock')}
+  else if (computerChoice ==='rock' && playerSelection =='scissors')
+  {console.log('You lose, rock breaks scissors')}
+   else if (computerChoice ==='paper' && playerSelection =='scissors')
+  {console.log('You win, scissors cuts paper')}
+  else if (computerChoice ==='paper' && playerSelection =='paper')
+  {console.log('It is a draw. You both choose paper.')}
+  else if (computerChoice ==='scissors' && playerSelection =='paper')
+  {console.log('You lose, scissors cuts paper')}
+  else 
+  {console.log('It is a draw. You both choose scissors.')}
+}
